6cc2c88a40e234fc4bf9165422ff0f7290fafb86
Rodrigo López-Romero Guijarro <rlromero@gmv.com>
2017-09-20 12:08:07 +0200

    Add unit tests for videoPresenter

--- DancingStepsTests/VideosPresenterTests.swift
+++ DancingStepsTests/VideosPresenterTests.swift
@@ -12,52 +12,91 @@ import RxSwift
 
 class VideosPresenterTests: XCTestCase {
     
+    // MARK: - CONFIGURATION
+    
     var sut: VideosPresenter!
     var view: TestableVideosTableVC!
+    var cellView: TestableVideoCellView!
+    var getStylesUseCase: TestableGetDanceStylesUseCase!
+    var getVideosUseCase: TestableGetVideosUseCase!
     
     override func setUp() {
         super.setUp()
         view = TestableVideosTableVC()
-        sut = VideosPresenter(getVideosUseCase: TestableGetVideosUseCase(), getDanceStylesUseCase: TestableGetDanceStylesUseCase())
+        cellView = TestableVideoCellView()
+        getStylesUseCase = TestableGetDanceStylesUseCase()
+        getVideosUseCase = TestableGetVideosUseCase()
+        sut = VideosPresenter(getVideosUseCase: getVideosUseCase, getDanceStylesUseCase: getStylesUseCase)
         sut.videosTableVC = view
     }
     
     override func tearDown() {
         view = nil
+        getStylesUseCase = nil
+        getVideosUseCase = nil
         sut = nil
         super.tearDown()
     }
+    
+    // MARK: - TESTS
     
     func testTrueTest() {
         XCTAssertTrue(true)
     }
     
-    // TODO: PROGRAM TESTS FOR THE PRESENTER HERE !!!!!!!!
+    func testViewReadyLoadsVideosFromUseCase() {
+        sut.viewIsReady()
+        XCTAssertTrue(sut.videos.value.count == 3)
+    }
+    
+    func testGetDanceStylesLoadsStyles() {
+        sut.getDanceStyles()
+        XCTAssertTrue(sut.styles.value.count == 2)
+    }
+    
+    func testConfigureCellCallsViewDisplay() {
+        sut.viewIsReady()
+        sut.getDanceStyles()
+        sut.configure(cell: cellView, forSectionAt: 0, forRowAt: 0)
+        XCTAssertEqual(cellView.cellName, TestData.videos[0].title)
+    }
+    
+    func testConfigureVideoScreenCallsView() {
+        sut.viewIsReady()
+        sut.getDanceStyles()
+        sut.configureVideoScreen(forIndexAt: IndexPath(row: 0, section: 0))
+        XCTAssertTrue(view.displayVideoScreenInvoked)
+    }
+    
+    // MARK: - MOCKS
     
     class TestableVideosTableVC : VideosTableVCProtocol {
+        var displayVideoScreenInvoked = false
         
         func displayVideoScreen(withVideoTitle title: String) {
-            
+            displayVideoScreenInvoked = true
         }
     }
     
-    class TestableGetVideosUseCase : GetVideosProtocol {
-        func getAllVideos() -> Observable<[Video]> {
-            let observable = Observable<[Video]>.create { observer in
-                observer.onCompleted()
-                return Disposables.create()
-            }
-            return observable
+    class TestableVideoCellView : VideoCellView {
+        var cellName = ""
+        
+        func display(name: String) {
+            cellName = name
         }
     }
     
     class TestableGetDanceStylesUseCase: GetDanceStylesProtocol {
         func getDanceStyles() -> Observable<[Style]> {
-            let observable = Observable<[Style]>.create { observer in
-                observer.onCompleted()
-                return Disposables.create()
-            }
-            return observable
+            return Utils.myFrom(TestData.styles)
         }
     }
+    
+    class TestableGetVideosUseCase : GetVideosProtocol {
+        func getAllVideos() -> Observable<[Video]> {
+            return Utils.myFrom(TestData.videos)
+        }
+    }
+    
+    
 }

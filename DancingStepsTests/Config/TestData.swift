a6918985a03c7c8a271c97069f5ec0b5d6e70fa4
Rodrigo López-Romero Guijarro <rlromero@gmv.com>
2017-09-20 16:14:41 +0200

    Add unit tests for CongressesPresenter

--- DancingStepsTests/Config/TestData.swift
+++ DancingStepsTests/Config/TestData.swift
@@ -11,9 +11,17 @@ import Foundation
 @testable import DancingSteps
 
 struct TestData {
-    static let styles: [Style] = [Style(name: "Salsa", country: "Cuba"),
+    static let styles: [Style] =
+                        [Style(name: "Salsa", country: "Cuba"),
                          Style(name: "Flamenco", country: "España")]
-    static let videos: [Video] = [Video(id: "1", title: "1", datetime: Date(), videoDescription: "DESCRIPTION TEST", url: "Deprecated", style: styles[0]),
+    
+    static let videos: [Video] =
+                        [Video(id: "1", title: "1", datetime: Date(), videoDescription: "DESCRIPTION TEST", url: "Deprecated", style: styles[0]),
                          Video(id: "2", title: "2", datetime: Date(), videoDescription: "DESCRIPTION TEST", url: "Deprecated", style: styles[1]),
                          Video(id: "3", title: "3", datetime: Date(), videoDescription: "DESCRIPTION TEST", url: "Deprecated", style: styles[0])]
+    
+    static let congresses: [Congress] =
+                        [Congress(name: "Cong1", organizer: "Me", startDate: Date(), endDate: Date(), imageUrl: "testingUrl/Cong1", descriptionText: "Description Cong1"),
+                         Congress(name: "Cong2", organizer: "You", startDate: Date(), endDate: Date(), imageUrl: "testingUrl/Cong2", descriptionText: "Description Cong1"),
+                         Congress(name: "Cong3", organizer: "Us", startDate: Date(), endDate: Date(), imageUrl: "testingUrl/Cong3", descriptionText: "Description Cong1")]
 }
